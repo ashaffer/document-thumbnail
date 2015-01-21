2329d049d27af111e2686206ce902f08bf1e96a8
Andrew Shaffer <andrew@weo.io>
2015-01-21 13:05:11 -0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var pdfThumb = require('pdf-thumbnail');
 var fs = require('fs');
 
 module.exports = function docThumb(input, output, cb) {
-  convertToPdf(input, function(err, buf) {
+  convertToPdf(input, {}, function(err, buf) {
     if(err) throw err;
     pdfThumb(buf)
       .pipe(fs.createWriteStream(output))

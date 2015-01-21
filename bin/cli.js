9745e01c0e37f0f14a0bb21632bc9e39bcfba246
Andrew Shaffer <andrew@weo.io>
2015-01-20 16:50:02 -0800

    next

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,9 @@
 var docThumb = require('../');
-var idx = process.argv[0] === 'node' || process.argv[0] === 'nodejs' ? 1 : 0;
+var idx = process.argv[0] === 'node' || process.argv[0] === 'nodejs' ? 2 : 1;
 var input = process.argv[idx];
 var output = process.argv[idx + 1];
 
-console.log('in out', input, output);
+console.log('in out', process.argv, idx, input, output);
 
 
 docThumb(input, output, function(err) {

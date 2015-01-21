2d129bab7d5fb46f34947bc7f21db490d7a1d22c
Andrew Shaffer <andrew@weo.io>
2015-01-21 13:14:24 -0800

    update

--- bin/cli.js
+++ bin/cli.js
@@ -2,9 +2,10 @@ var docThumb = require('../');
 var idx = process.argv[0] === 'node' || process.argv[0] === 'nodejs' ? 2 : 1;
 var input = process.argv[idx];
 var output = process.argv[idx + 1];
+var fs = require('fs');
 
-docThumb(input, output, function(err) {
-  console.log('here', err);
+docThumb(input, function(err, buf) {
   if(err) throw err;
+  fs.writeFileSync(output, buf);
   console.log('completed!');
 });
